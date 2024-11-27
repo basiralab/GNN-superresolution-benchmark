258ce537b8c82eeab6bd46e9dd04e2a2b754517c
Euan Goodbrand <egoodbrand@mercedesamgf1.com>
2024-11-27 14:24:07 +0000

    Team 6 Code added- removed identifiable information

---
+++ Team 6/gan/initializations.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from set_seed import set_seed
+
+set_seed(42)
+
+
+def weight_variable_glorot(output_dim, input_dim=None):
+    if input_dim is None:
+        input_dim = output_dim
+    init_range = np.sqrt(6.0 / (input_dim + output_dim))
+    initial = np.random.uniform(-init_range, init_range,
+                                (input_dim, output_dim))
+    return initial
---
+++ Team 6/main.py
@@ -0,0 +1,62 @@
+# Imports
+import torch
+from sklearn.model_selection import train_test_split
+
+from gan.config import Args
+from gan.model import GUS
+from gan.preprocessing import degree_normalisation, preprocess_data
+from gan.train import train as train_model
+from utils import load_csv_files, three_fold_cross_validation, save_csv_prediction, plot_evaluations
+from set_seed import set_seed
+from evaluation import load_random_files
+
+
+# Configurations
+# Set a fixed random seed for reproducibility across multiple libraries
+random_seed = 42
+set_seed(random_seed)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("CUDA is available. Using GPU.")
+else:
+    device = torch.device("cpu")
+    print("CUDA not available. Using CPU.")
+
+# preprocessing of the data
+args = Args()
+args.device = device
+args.normalisation_function = degree_normalisation
+
+splits = load_random_files(args,return_matrix=True)
+
+# print the args
+print(args)
+
+#lr_train, lr_test, hr_train, hr_test = train_test_split(lr_train_data, hr_train_data, test_size=0.2, random_state=42)
+
+
+def model_init():
+    model = GUS(args.ks, args).to(device)
+    return model
+
+
+# run the 3-fold cross-validation
+cv_scores = three_fold_cross_validation(model_init, splits, random_state=random_seed,
+                                        verbose=True, prediction_vector=False, label_vector=False)
+
+#print(f"The average over the 3 folds is: {torch.mean(torch.tensor(cv_scores), dim=0)}")
+
+# Train the model on the whole dataset
+#model = GUS(args.ks, args).to(device)
+#model = train_model(model, lr_train_data, hr_train_data, args=args, verbose=True)  # train on the whole dataset
+# save the trained model
+#torch.save(model, "model.pth")
+
+# Test the model on the test set
+#lr_test_A, lr_test_X = preprocess_data(lr_test_data, args)
+#lr_test_data = torch.stack([lr_test_A, lr_test_X], dim=1)
+
+#lr_test_predictions = model.predict(lr_test_data)
+#save_csv_prediction(lr_test_predictions, logs=True, pred_matrix=True)
+print(cv_scores)
