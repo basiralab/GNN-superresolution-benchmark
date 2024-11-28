62cac032760317b32a86e253d7b842aabefa65ae
Euan Goodbrand <egoodbrand@mercedesamgf1.com>
2024-11-28 10:34:40 +0000

    Formatting code

--- Team 19/evaluation_measures.py
+++ Team 19/evaluation_measures.py
@@ -1,10 +1,8 @@
 from MatrixVectorizer import MatrixVectorizer
-
-import torch
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.metrics import mean_absolute_error
 from scipy.stats import pearsonr
 from scipy.spatial.distance import jensenshannon
 
